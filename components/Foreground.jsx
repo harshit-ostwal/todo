d63cbd94809dd4c999c277d03b12d2166b7f4672
Harshit Jain <113440545+harshit-ostwal@users.noreply.github.com>
2024-01-02 10:38:40 +0530

    Update Foreground.jsx

--- components/Foreground.jsx
+++ components/Foreground.jsx
@@ -10,6 +10,13 @@ export default function Foreground() {
         <>
             <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 p-2 lg:p-5 flex-wrap overflow-hidden overflow-y-scroll">
                 <Card reference={ref} />
+                <Card reference={ref} />
+                <Card reference={ref} />
+                <Card reference={ref} />
+                <Card reference={ref} />
+                <Card reference={ref} />
+                <Card reference={ref} />
+                <Card reference={ref} />
             </div>
         </>
     )
